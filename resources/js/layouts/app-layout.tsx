5b5e0ebd4fe060f441766ac9256f8f2c9207d57e
Rafael Hernández <145055756+al070698@users.noreply.github.com>
2026-05-13 10:59:16 -0600

    Update app-layout.tsx

--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -10,15 +10,7 @@ export default function AppLayout({
 }) {
     return (
         <AppLayoutTemplate breadcrumbs={breadcrumbs}>
-    <div className="flex min-h-screen flex-col">
-        <main className="flex-1">
             {children}
-        </main>
-
-        <footer className="border-t border-sidebar-border/70 px-6 py-4 text-center text-sm text-muted-foreground">
-            © 2026 Rafael Gilberto Hernández Jiménez
-        </footer>
-    </div>
         </AppLayoutTemplate>
     );
 }
